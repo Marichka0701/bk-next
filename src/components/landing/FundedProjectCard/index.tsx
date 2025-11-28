b9bef95ab568448b6cd3ea5a7e16461e024e7988
Marichka Tanechnyk <tanechnyk.marichka07@gmail.com>
2025-11-28 20:32:15 +0200

    fix: add financing badge

--- src/components/landing/FundedProjectCard/index.tsx
+++ src/components/landing/FundedProjectCard/index.tsx
@@ -12,15 +12,26 @@ export function FundedProjectCard({
   item: { imageSrc, imageAlt, title, description, budget, isFlexibleFinancing },
 }: Props) {
   return (
-    <div className="w-full bg-secondary-background rounded-xl py-6 px-4.5 flex flex-col">
+    <div className="relative w-full bg-secondary-background rounded-xl py-6 px-4.5 flex flex-col">
       <Image
         src={imageSrc}
         alt={imageAlt}
         width={340}
         height={220}
         draggable={false}
-        className="h-[220px] rounded-lg"
+        className="w-full h-[220px] rounded-lg object-cover"
       />
+
+      {isFlexibleFinancing && (
+        <div className="min-h-8 px-3 absolute top-9 left-9 bg-brand-secondary-100 rounded-full flex justify-center items-center">
+          <BodyText
+            variant="14Semibold"
+            className="text-secondary-orange-100!"
+          >
+            Fleksibel finansiering
+          </BodyText>
+        </div>
+      )}
 
       <div className="mt-7 flex-1 flex flex-col justify-between gap-12">
         <div className="space-y-4">
